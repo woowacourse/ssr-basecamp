66ab49b97da19bd951ca99672c87e0211ece9483
novice0840 <magnolia0840@gmail.com>
2024-10-01 03:49:07 +0900

    feat: modal 페이지에 referer 전달

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -22,28 +22,18 @@ const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-const renderMovies = async (res, filter) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-
-  try {
-    const response = await fetch(url(filter), options);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    const movies = data.results;
-    const moviesHTML = `
+const renderMovies = (movies) => {
+  return `
     ${movies
       .map(
         (movie) => `
       <li>
-        <a href="/detail/${movie.id}>${movie.title}">
+        <a href="/detail/${movie.id}">
           <div class="item">
             <img class="thumbnail" src="https://image.tmdb.org/t/p/w440_and_h660_face///${movie.poster_path}" alt="${movie.title}">
             <div class="item-desc">
               <p class="rate">
-                <img src="http://localhost:3000/assets/images/star_empty.png" class="star">
+                <img src="/assets/images/star_empty.png" class="star">
                 <span>${movie.vote_average}</span>
               </p>
               <strong>${movie.title}</strong>
@@ -55,14 +45,34 @@ const renderMovies = async (res, filter) => {
       )
       .join("")}
     `;
+};
+
+const getMovies = async (filter) => {
+  const response = await fetch(url(filter), options);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.results;
+};
+
+const filterMapper = {
+  "now-playing": "상영 중",
+  popular: "인기순",
+  "top-rated": "평점순",
+  upcoming: "상영 예정",
+};
+
+const renderHTML = async (res, filter, modal = false, from) => {
+  console.log(modal, from);
+  try {
+    const templatePath = path.join(__dirname, "../../views", "index.html");
+    const movies = await getMovies(filter);
+    const moviesHTML = renderMovies(movies);
     const template = fs.readFileSync(templatePath, "utf-8");
     const bestMovie = movies[0];
-    const filterMapper = {
-      "now-playing": "상영 중",
-      popular: "인기순",
-      "top-rated": "평점순",
-      upcoming: "상영 예정",
-    };
+
     const filterLink = filter.replace("_", "-");
     const renderedHTML = template
       .replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML)
@@ -94,23 +104,33 @@ const renderMovies = async (res, filter) => {
 };
 
 // 상영 중
-router.get([",", "/now-playing"], async (_, res) => {
-  renderMovies(res, "now_playing");
+router.get(["/", "/now-playing"], async (_, res) => {
+  renderHTML(res, "now_playing");
 });
 
 // 인기순
 router.get("/popular", (_, res) => {
-  renderMovies(res, "popular");
+  renderHTML(res, "popular");
 });
 
 // 평점순
 router.get("/top-rated", (_, res) => {
-  renderMovies(res, "top_rated");
+  renderHTML(res, "top_rated");
 });
 
 // 상영 예정
 router.get("/upcoming", (_, res) => {
-  renderMovies(res, "upcoming");
+  renderHTML(res, "upcoming");
+});
+
+router.get("/detail/:id", (req, res) => {
+  const referer = req.get("Referer");
+
+  if (referer) {
+    renderHTML(res, "upcoming", true, referer.split("/").at(-1));
+  } else {
+    renderHTML(res, "upcoming", true);
+  }
 });
 
 export default router;
