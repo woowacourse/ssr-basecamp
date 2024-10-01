248cd198149cf4182dceb6a23bd8dd06fc87056f
river <130737187+0jenn0@users.noreply.github.com>
2024-10-02 02:09:02 +0900

    feat(render): 영화 상세 정보 모달 렌더 함수 추가

--- ssr/server/utils/render.js
+++ ssr/server/utils/render.js
@@ -31,11 +31,11 @@ export const renderHeader = (movieItem) => {
     <div class="overlay" aria-hidden="true"></div>
     <div class="top-rated-container">
       <h1 class="logo">
-        <img src="../assets/images/logo.png" alt="MovieList" />
+        <img src="/assets/images/logo.png" alt="MovieList" />
       </h1>
       <div class="top-rated-movie">
         <div class="rate">
-          <img src="../assets/images/star_empty.png" class="star" />
+          <img src="/assets/images/star_empty.png" class="star" />
           <span class="rate-value">${movieItem.vote_average}</span>
         </div>
         <div class="title">${movieItem.title}</div>
@@ -45,4 +45,44 @@ export const renderHeader = (movieItem) => {
   </div>
 </header>
         `;
+};
+
+export const renderMovieDetailModal = (movieDetail) => {
+  return /*html*/ `
+      <div class="modal-background active" id="modalBackground">
+        <div class="modal">
+        <button class="close-modal" id="closeModal"><img src="/assets/images/modal_button_close.png" /></button>
+        <div class="modal-container">
+          <div class="modal-image">
+            <img src="https://image.tmdb.org/t/p/original/${
+              movieDetail.poster_path
+            }.jpg" />
+          </div>
+          <div class="modal-description">
+            <h2>${movieDetail.title}</h2>
+            <p class="category">${
+              movieDetail.release_date.split("-")[0]
+            } · ${movieDetail.genres.map((genre) => genre.name).join(", ")}</p>
+            <p class="rate"><img src="/assets/images/star_filled.png" class="star" /><span>${
+              movieDetail.vote_average
+            }</span></p>
+            <hr />
+            <p class="detail">
+              ${movieDetail.overview}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 모달 창 닫기 스크립트 -->
+    <script>
+      const modalBackground = document.getElementById("modalBackground");
+      const closeModal = document.getElementById("closeModal");
+      document.addEventListener("DOMContentLoaded", () => {
+        closeModal.addEventListener("click", () => {
+          modalBackground.classList.remove("active");
+        });
+      });
+    </script>
+  `;
 };
