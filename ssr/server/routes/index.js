875d1361e3b4191d51677a067fe78ee97987a62d
ss0526100 <ss0526100@gmail.com>
2024-10-01 16:55:06 +0900

    feat: index 기준 라우터 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,21 +1,30 @@
-import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { Router } from 'express';
+import createIndex from '../../views/createIndex.js';
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
-
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
-
-  res.send(renderedHTML);
+router.get('/', async (_, res) => {
+  const html = await createIndex('nowPlaying');
+  res.send(html);
 });
 
+router.get('/now-playing', async (_, res) => {
+  const html = await createIndex('nowPlaying');
+  res.send(html);
+});
+
+router.get('/popular', async (_, res) => {
+  const html = await createIndex('popular');
+  res.send(html);
+});
+
+router.get('/top-rated', async (_, res) => {
+  const html = await createIndex('topRated');
+  res.send(html);
+});
+
+router.get('/upcoming', async (_, res) => {
+  const html = await createIndex('upcoming');
+  res.send(html);
+});
 export default router;
