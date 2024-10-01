89a6f87b6c92f0ef01e32f700e61c26d074a58fc
river <130737187+0jenn0@users.noreply.github.com>
2024-10-02 01:05:14 +0900

    refactor(tmdb): movie fetch 함수 추상화

--- ssr/server/utils/tmdb.js
+++ ssr/server/utils/tmdb.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import { TMDB_MOVIE_LISTS, FETCH_OPTIONS } from "../../src/constant.js";
 
-export const fetchMovies = async () => {
-  const response = await fetch(TMDB_MOVIE_LISTS.POPULAR, FETCH_OPTIONS);
+export const fetchMovies = async (movieListType) => {
+  const response = await fetch(TMDB_MOVIE_LISTS[movieListType], FETCH_OPTIONS);
   return await response.json();
 };
