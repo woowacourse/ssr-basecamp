72be569734d55c8ae1ff441bc58061247869e07b
ss0526100 <ss0526100@gmail.com>
2024-10-01 16:57:30 +0900

    feat(createMovieThumbnail):평점 한자리수로 고정

--- ssr/views/createMovieThumbnail.js
+++ ssr/views/createMovieThumbnail.js
@@ -34,7 +34,7 @@ export default function createMovieThumbnail(movie) {
       <div class='item-desc'>
         <p class='rate'>
           <img src='../assets/images/star_empty.png' class='star' />
-          <span>${movie.vote_average}</span>
+          <span>${movie.vote_average.toFixed(1)}</span>
         </p>
         <strong>${movie.title}</strong>
       </div>
