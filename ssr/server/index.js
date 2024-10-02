7d488a4deb5dcc72810891f1d1f95744e58797da
bmj0053 <bmj05378@gmail.com>
2024-10-03 00:45:46 +0900

    feat: 현재상영중 목록 불러오기, 이미지 설정

--- ssr/server/index.js
+++ ssr/server/index.js
@@ -1,9 +1,10 @@
-import "./config.js";
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import './config.js';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import movieRouter from "./routes/index.js";
+import nowPlaying from './routes/nowPlaying.js';
+import popular from './routes/popular.js';
 // import membersRouter from "./routes/members.js"; // 본 미션 참고를 위한 코드이며 사전 미션에서는 사용하지 않습니다.
 
 const app = express();
@@ -12,9 +13,14 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use("/assets", express.static(path.join(__dirname, "../public")));
+app.use('/assets', express.static(path.join(__dirname, '../public')));
+app.use(
+  '/images',
+  express.static(path.join(__dirname, '../../ssr/public/images'))
+);
 
-app.use("/", movieRouter);
+app.use('/', nowPlaying);
+app.use('/popular', popular);
 // app.use("/members", membersRouter); // 본 미션 참고를 위한 코드이며 사전 미션에서는 사용하지 않습니다.
 
 // Start server
