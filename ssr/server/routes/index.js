c32e5c08012c749cccecd305e8f4cfe53943809f
김진호 <rlawlsgh1227@gmail.com>
2024-10-02 15:36:34 +0900

    feat: 인기있는 영화 목록 불러오는 기능 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -2,15 +2,26 @@ import { Router } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "../Constant.js";
+import MovieView from "../../views/movie/MovieView.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
+const loadMovies = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.popular, FETCH_OPTIONS);
+  const data = await response.json();
+
+  return data.results;
+};
+
+router.get("/", async (_, res) => {
   const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+  const movies = await loadMovies();
+
+  const moviesHTML = movies.map((movie) => MovieView(movie)).join("");
 
   const template = fs.readFileSync(templatePath, "utf-8");
   const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
---
+++ ssr/views/movie/MovieView.js
@@ -0,0 +1,19 @@
+import { TMDB_THUMBNAIL_URL } from "../../server/Constant.js";
+
+const MovieView = (movie) => {
+  return `<li>
+    <div class="item">
+      <img class="thumbnail" src="${`${TMDB_THUMBNAIL_URL}${movie.poster_path}`}" alt="${movie.title}" />
+      <div class="item-desc">
+        <p class="rate">
+          <img src="/assets/images/star_empty.png"" class="star" />
+          <span>${movie.vote_average.toFixed(1)}</span>
+        </p>
+        <strong>${movie.title}</strong>
+      </div>
+    </div>
+  </li>
+`;
+};
+
+export default MovieView;
