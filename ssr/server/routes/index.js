d2c9c86cedc04a9d416d0f31f2d541f12a2fa189
rbgksqkr <rbgks1937@naver.com>
2024-10-02 14:40:02 +0900

    feat: 영화 목록 탭에 알맞은 콘텐츠가 로드되도록 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,11 +1,44 @@
 import { Router } from "express";
 import { renderMoviePage } from "../src/render/renderMoviePage.js";
-import { fetchMovies } from "../src/apis/movie.js";
+import {
+  fetchMoviesNowPlaying,
+  fetchMoviesPopular,
+  fetchMoviesTopRated,
+  fetchMoviesUpcoming,
+} from "../src/apis/movie.js";
 
 const router = Router();
 
 router.get("/", async (_, res) => {
-  const movies = await fetchMovies();
+  const movies = await fetchMoviesNowPlaying();
+  const renderedHTML = renderMoviePage(movies);
+
+  res.send(renderedHTML);
+});
+
+router.get("/now-playing", async (_, res) => {
+  const movies = await fetchMoviesNowPlaying();
+  const renderedHTML = renderMoviePage(movies);
+
+  res.send(renderedHTML);
+});
+
+router.get("/popular", async (_, res) => {
+  const movies = await fetchMoviesPopular();
+  const renderedHTML = renderMoviePage(movies);
+
+  res.send(renderedHTML);
+});
+
+router.get("/top-rated", async (_, res) => {
+  const movies = await fetchMoviesTopRated();
+  const renderedHTML = renderMoviePage(movies);
+
+  res.send(renderedHTML);
+});
+
+router.get("/upcoming", async (_, res) => {
+  const movies = await fetchMoviesUpcoming();
   const renderedHTML = renderMoviePage(movies);
 
   res.send(renderedHTML);
--- ssr/server/src/apis/movie.js
+++ ssr/server/src/apis/movie.js
@@ -1,7 +1,25 @@
 import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "../constants/index.js";
 
-export const fetchMovies = async () => {
+export const fetchMoviesNowPlaying = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.NOW_PLAYING, FETCH_OPTIONS);
+
+  return await response.json();
+};
+
+export const fetchMoviesPopular = async () => {
   const response = await fetch(TMDB_MOVIE_LISTS.POPULAR, FETCH_OPTIONS);
+
+  return await response.json();
+};
+
+export const fetchMoviesTopRated = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.TOP_RATED, FETCH_OPTIONS);
+
+  return await response.json();
+};
+
+export const fetchMoviesUpcoming = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.UPCOMING, FETCH_OPTIONS);
 
   return await response.json();
 };
