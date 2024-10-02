7b2cac39c09d56e5916001d5da20c8e4012fc227
hanyoung <phanyoung@naver.com>
2024-10-02 17:07:29 +0900

    feat: movieList 페이지 기능 구현 완료

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,19 +1,31 @@
 import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { generateMovieListTemplate } from "../templateGenerators.js";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+router.get("/", async (_, res) => {
+  const renderedHTML = await generateMovieListTemplate();
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  res.send(renderedHTML);
+});
+router.get("/now-playing", async (_, res) => {
+  const renderedHTML = await generateMovieListTemplate("nowPlaying");
+
+  res.send(renderedHTML);
+});
+router.get("/popular", async (_, res) => {
+  const renderedHTML = await generateMovieListTemplate("popular");
+
+  res.send(renderedHTML);
+});
+router.get("/top-rated", async (_, res) => {
+  const renderedHTML = await generateMovieListTemplate("topRated");
+
+  res.send(renderedHTML);
+});
+
+router.get("/upcoming", async (_, res) => {
+  const renderedHTML = await generateMovieListTemplate("upcoming");
 
   res.send(renderedHTML);
 });
