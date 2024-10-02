83a8ebfdeec400f93569a8ff1e1608b522cd62d6
river <130737187+0jenn0@users.noreply.github.com>
2024-10-02 10:57:47 +0900

    fix(page): now-playing 라우터 누락 수정

--- ssr/server/routes/page.js
+++ ssr/server/routes/page.js
@@ -3,7 +3,9 @@ import { handleMovieRequest } from "../controller/handleMovieRequest.js";
 
 const router = Router();
 
-router.get("/", (_, res) => handleMovieRequest(res, "NOW_PLAYING"));
+router.get(["/", "/now-playing"], (_, res) =>
+  handleMovieRequest(res, "NOW_PLAYING")
+);
 router.get("/popular", (_, res) => handleMovieRequest(res, "POPULAR"));
 router.get("/top-rated", (_, res) => handleMovieRequest(res, "TOP_RATED"));
 router.get("/upcoming", (_, res) => handleMovieRequest(res, "UPCOMING"));
