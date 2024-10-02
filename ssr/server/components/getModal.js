08158518f7eea0706d88fe23916ff48af62a35dd
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-02 23:08:10 +0900

    feat: 모달 컴포넌트 구현

---
+++ ssr/server/components/getModal.js
@@ -0,0 +1,34 @@
+import { TMDB_ORIGINAL_URL } from "../../utils/constants.js";
+
+const round = (value, decimals = 0) => {
+  const factor = 10 ** decimals;
+
+  return Math.round(value * factor) / factor;
+};
+
+export default function getModal(movieDetail) {
+  return `<div class="modal-background active" id="modalBackground">
+      <div class="modal">
+        <button class="close-modal" id="closeModal">
+          <img src="/assets/images/modal_button_close.png" />
+        </button>
+        <div class="modal-container">
+          <div class="modal-image">
+            <img src="${TMDB_ORIGINAL_URL + movieDetail.poster_path}" />
+          </div>
+          <div class="modal-description">
+            <h2>${movieDetail.title}</h2>
+            <p class="category">
+              ${movieDetail.release_date.split("-")[0]} · ${movieDetail.genres.map(({ name }) => name).join(", ")}
+            </p>
+            <p class="rate">
+              <img src="/assets/images/star_empty.png" class="star" />
+              <span>${round(movieDetail.vote_average, 1)}</span>
+            </p>
+            <hr />
+            <p class="detail">${movieDetail.overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+}
