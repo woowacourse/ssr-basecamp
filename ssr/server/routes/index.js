cbf98e5db2ad5e511a02a223489053140f59c05d
badahertz52 <badahertz52@gmail.com>
2024-10-02 17:18:45 +0900

    refactor: 렌더링한 html을 만드는 함수 분리

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { fetchMovies } from "../apis/movie.js";
+import { fetchMovies, loadMovieDetail } from "../apis/movie.js";
 import {
   renderBestMovie,
+  renderModal,
   renderMovieItem,
   renderTabList,
 } from "../utils/renderHelper.js";
@@ -15,32 +16,34 @@ const __dirname = path.dirname(__filename);
 
 const router = Router();
 
+const generateMovieListHTML = async category => {
+  const templatePath = path.join(__dirname, "../../views", "index.html");
+
+  const movieData = await fetchMovies(category);
+
+  // HTML 변경
+  const template = fs.readFileSync(templatePath, "utf-8");
+  const renderedTabList = template.replace(
+    "<!--${TAB_LIST}-->",
+    renderTabList(CONTAINER_TAB_LIST[category])
+  );
+
+  const movieListHTML = movieData.results.map(renderMovieItem).join("");
+
+  const bestMovieHTML = renderBestMovie(movieData.results[0], renderedTabList);
+
+  const renderedHTML = bestMovieHTML.replace(
+    "<!--${MOVIE_ITEMS_PLACEHOLDER}-->",
+    movieListHTML
+  );
+
+  return renderedHTML;
+};
+
 const routerMovieList = async (res, category) => {
   try {
-    const templatePath = path.join(__dirname, "../../views", "index.html");
-
-    const movieData = await fetchMovies(category);
-
-    // HTML 변경
-    const template = fs.readFileSync(templatePath, "utf-8");
-    const renderedTabList = template.replace(
-      "<!--${TAB_LIST}-->",
-      renderTabList(CONTAINER_TAB_LIST[category])
-    );
-
-    const moviesHTML = movieData.results.map(renderMovieItem).join("");
-
-    const bestMovieHTML = renderBestMovie(
-      movieData.results[0],
-      renderedTabList
-    );
-
-    const renderedHTML = bestMovieHTML.replace(
-      "<!--${MOVIE_ITEMS_PLACEHOLDER}-->",
-      moviesHTML
-    );
-
-    res.send(renderedHTML);
+    const html = await generateMovieListHTML(category);
+    res.send(html);
   } catch (error) {
     console.error("Error rendering page:", error);
     res.status(500).send("Internal Server Error");
