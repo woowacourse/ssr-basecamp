324e58df7d6a74e67021132f66888b484ef35069
Largopie <jae990320@kakao.com>
2024-10-02 22:18:06 +0900

    feat: 인기영화 목록 html 요소 교체해서 전달하도록 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -2,18 +2,38 @@ import { Router } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { getPopularMovies } from "../../src/api/index.js";
+import { renderMovieList } from "../../src/components/movieList.js";
+import { renderHeader } from "../../src/components/header.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
+router.get("/", async (_, res) => {
   const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+  const popularMovies = await getPopularMovies();
+  const movieList = popularMovies.results.map(({ id, title, poster_path, vote_average }) => ({
+    id,
+    title,
+    thumbnail: poster_path,
+    rate: vote_average.toFixed(1),
+  }));
+
+  const bestMovie = {
+    title: popularMovies.results[0].title,
+    background: popularMovies.results[0].backdrop_path,
+    rate: popularMovies.results[0].vote_average.toFixed(1),
+  };
+
+  const headerHTML = renderHeader(bestMovie);
+  const moviesHTML = renderMovieList(movieList);
 
   const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  const renderedHTML = template
+    .replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML)
+    .replace("<!--${HEADER_PLACEHOLDER}-->", headerHTML);
 
   res.send(renderedHTML);
 });
