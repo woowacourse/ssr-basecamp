2539298a8ad6deabf72636fa2f1fda02a15cb772
Largopie <jae990320@kakao.com>
2024-10-03 01:16:02 +0900

    feat: movieDetail api 추가

--- ssr/src/api/index.js
+++ ssr/src/api/index.js
@@ -1,11 +1,17 @@
 import fetch from "node-fetch";
-import { BASE_URL, FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "./constant.js";
+import { BASE_URL, FETCH_OPTIONS } from "./constant.js";
 
 export const getMovies = async (endpoint) => {
   const response = await fetch(
     `${BASE_URL}/${endpoint.replaceAll("-", "_")}?language=ko-KR&page=1`,
     FETCH_OPTIONS,
   );
+
+  return await response.json();
+};
+
+export const getMovieDetail = async (movieId) => {
+  const response = await fetch(`${BASE_URL}/${movieId}?language=ko-KR&page=1`, FETCH_OPTIONS);
 
   return await response.json();
 };
