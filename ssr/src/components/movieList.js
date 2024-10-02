5988595b973d4ac4b2481105d534926256c0c88b
Largopie <jae990320@kakao.com>
2024-10-02 19:57:59 +0900

    feat(movieList): 컴포넌트 구현

---
+++ ssr/src/components/movieList.js
@@ -0,0 +1,21 @@
+export const renderMovieList = (movieItems = []) =>
+  movieItems
+    .map(
+      ({ id, title, thumbnail, rate }) => /*html*/ `
+      <li>
+        <a href="/detail/${id}">
+          <div class="item">
+            <img
+              class="thumbnail"
+              src="https://media.themoviedb.org/t/p/w440_and_h660_face/${thumbnail}"
+              alt="${title}"
+            />
+            <div class="item-desc">
+              <p class="rate"><img src="../assets/images/star_empty.png" class="star" /><span>${rate}</span></p>
+              <strong>${title}</strong>
+            </div>
+          </div>
+        </a>
+      </li>`,
+    )
+    .join("");
