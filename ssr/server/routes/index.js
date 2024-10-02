d85453e3d924fc39b9c53789feff6821c656d8aa
김진호 <rlawlsgh1227@gmail.com>
2024-10-03 00:32:57 +0900

    feat: 각 라우팅마다 다른 정보 불러오며, 선택된 탭 활성화 기능

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -4,35 +4,88 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "../Constant.js";
 import MovieItemView from "../../views/movieItem/MovieItemView.js";
-import TopRatedMovie from "../../views/topRatedMovie/TopRatedMovie.js";
+import TopRatedMovieView from "../../views/topRatedMovie/TopRatedMovieView.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-const loadMovies = async () => {
-  const response = await fetch(TMDB_MOVIE_LISTS.popular, FETCH_OPTIONS);
+const loadMovies = async (url) => {
+  const response = await fetch(url, FETCH_OPTIONS);
   const data = await response.json();
 
   return data.results;
 };
 
-router.get("/", async (_, res) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-  const movies = await loadMovies();
-
-  const moviesHTML = movies.map((movie) => MovieItemView(movie)).join("");
+const getTopRateMovie = (movies) => {
   const topRateMovie = movies.reduce((highest, movie) => {
     return movie.vote_average > highest.vote_average ? movie : highest;
   }, movies[0]);
 
-  const topRateMovieHTML = TopRatedMovie(topRateMovie);
+  return topRateMovie;
+};
+
+const tab = {
+  root: {
+    url: TMDB_MOVIE_LISTS.popular,
+    selectedTemplate: null,
+  },
+  nowPlaying: {
+    url: TMDB_MOVIE_LISTS.nowPlaying,
+    selectedTemplate: "<!--${NOW_PLAYING_TAB_SELECTED}-->",
+  },
+  popular: {
+    url: TMDB_MOVIE_LISTS.popular,
+    selectedTemplate: "<!--${POPULAR_TAB_SELECTED}-->",
+  },
+  topRated: {
+    url: TMDB_MOVIE_LISTS.topRated,
+    selectedTemplate: "<!--${TOP_RATED_TAB_SELECTED}-->",
+  },
+  upcoming: {
+    url: TMDB_MOVIE_LISTS.upcoming,
+    selectedTemplate: "<!--${UPCOMING_TAB_SELECTED}-->",
+  },
+};
+
+const makeHTMLTemplates = async ({ url, selectedTemplate }) => {
+  const templatePath = path.join(__dirname, "../../views", "index.html");
+  const movies = await loadMovies(url);
+
+  const moviesHTML = movies.map((movie) => MovieItemView(movie)).join("");
+  const topRateMovieHTML = TopRatedMovieView(getTopRateMovie(movies));
+
   const template = fs.readFileSync(templatePath, "utf-8");
   const renderedHTML = template
     .replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML)
     .replace("<!--${TOP_RATED_MOVIE_PLACEHOLDER}-->", topRateMovieHTML);
 
+  return selectedTemplate !== null ? renderedHTML.replace(selectedTemplate, "selected") : renderedHTML;
+};
+
+router.get("/", async (_, res) => {
+  const renderedHTML = await makeHTMLTemplates({ ...tab.root });
+  res.send(renderedHTML);
+});
+
+router.get("/now-playing", async (_, res) => {
+  const renderedHTML = await makeHTMLTemplates({ ...tab.nowPlaying });
+  res.send(renderedHTML);
+});
+
+router.get("/popular", async (_, res) => {
+  const renderedHTML = await makeHTMLTemplates({ ...tab.popular });
+  res.send(renderedHTML);
+});
+
+router.get("/top-rated", async (_, res) => {
+  const renderedHTML = await makeHTMLTemplates({ ...tab.topRated });
+  res.send(renderedHTML);
+});
+
+router.get("/upcoming", async (_, res) => {
+  const renderedHTML = await makeHTMLTemplates({ ...tab.upcoming });
   res.send(renderedHTML);
 });
 
