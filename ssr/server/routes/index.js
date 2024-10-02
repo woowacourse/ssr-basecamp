55019e22c5d4ff08432da954bed5ae1e191fb6a3
skiende74 <skiende74@gmail.com>
2024-10-02 19:02:16 +0900

    feat: 현재 표시되고있는 탭에 색상입히기. (+모달에서도)

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -4,19 +4,45 @@ import { renderMovieDetailPage } from "../entities/movieDetail.js";
 import { renderMoviePage } from "../entities/movies.js";
 
 const router = Router();
+const TAB_LIST = new Map([
+  ["/now-playing", "상영 중"],
+  ["/popular", "인기순"],
+  ["/top-rated", "평점순"],
+  ["/upcoming", "상영 예정"],
+]);
 
-const sendMoviePage = (movies) => async (_, res) => {
-  const renderedHTML = renderMoviePage(movies);
-  res.send(renderedHTML);
+const renderTabList = (tabList, curPath) => {
+  return Array.from(tabList.entries())
+    .map(
+      ([path, name]) => `<li>
+  <a href="${path}"
+    ><div class="tab-item ${path === curPath ? "selected" : ""}">
+      <h3>${name}</h3>
+    </div></a
+  >
+</li>`
+    )
+    .join("");
+};
+
+const sendMoviePage = (movies) => async (req, res) => {
+  const renderedHTML = renderMoviePage(movies).replace();
+  const tabListHTML = renderTabList(TAB_LIST, req.path);
+
+  const newRenderedHTML = renderedHTML.replace("<!--${TAB_LIST}-->", tabListHTML);
+
+  res.send(newRenderedHTML);
 };
 
 const sendMovieDetailPage = async (req, res) => {
   const id = req.params.id;
   const movies = await loadNowPlaying();
   const movieDetail = await loadMovieDetail(id);
-  console.log(movieDetail);
   const renderedHTML = renderMovieDetailPage(movies, movieDetail);
-  res.send(renderedHTML);
+  const tabListHTML = renderTabList(TAB_LIST, "/now-playing");
+
+  const newRenderedHTML = renderedHTML.replace("<!--${TAB_LIST}-->", tabListHTML);
+  res.send(newRenderedHTML);
 };
 
 router.get("/", sendMoviePage(await loadNowPlaying()));
