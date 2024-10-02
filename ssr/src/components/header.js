ab9833af93a13d42d616ff12bb8a9c088d3cdabc
Largopie <jae990320@kakao.com>
2024-10-02 22:16:23 +0900

    feat(header): 컴포넌트 분리

---
+++ ssr/src/components/header.js
@@ -0,0 +1,18 @@
+export const renderHeader = ({ title, background, rate }) => /*html*/ `
+  <header>
+    <div class="background-container" style="background-image: url('https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${background}')">
+      <div class="overlay" aria-hidden="true"></div>
+      <div class="top-rated-container">
+        <h1 class="logo"><img src="../assets/images/logo.png" alt="MovieList" /></h1>
+        <div class="top-rated-movie">
+          <div class="rate">
+            <img src="../assets/images/star_empty.png" class="star" />
+            <span class="rate-value">${rate}</span>
+          </div>
+          <div class="title">${title}</div>
+          <button class="primary detail">자세히 보기</button>
+        </div>
+      </div>
+    </div>
+  </header>
+`;
