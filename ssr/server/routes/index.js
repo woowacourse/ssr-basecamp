4761f6835675b9311c216e8224c80fcc1d99e455
cys4585 <dev.youngsu@gmail.com>
2024-10-03 18:12:47 +0900

    feat: 영화 목록 페이지 구현 - CSR과 동일하게 렌더링되도록 API 연결 - API로 얻어온 데이터를 index.html의 텡플릿 변수(?)에 바인딩 - router.get으로 다른 주소도 페이지 렌더링, 다른 API 연결 - 선택한 탭 활성화되도록 index.html 변수 추가

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -8,14 +8,172 @@ const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
+router.get("/", async (_, res) => {
   const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
-
   const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+
+  const movies = await loadMovies(TMDB_MOVIE_LISTS.nowPlaying);
+  const movie = movies[0] ?? {
+    id: -1,
+    title: "",
+    bannerUrl: "",
+    vote_average: 0,
+  };
+
+  const renderedHTML = template
+    .replace("${MOVIE_ITEMS_PLACEHOLDER}", getMovieItemHTML(movies))
+    .replace("${background-container}", TMDB_BANNER_URL + movie.backdrop_path)
+    .replace("${bestMovie.rate}", round(movie.vote_average, 1))
+    .replace("${bestMovie.title}", movie.title)
+    .replace("${isSelectedNowPlaying}", "selected");
 
   res.send(renderedHTML);
 });
+
+router.get("/now-playing", async (_, res) => {
+  const templatePath = path.join(__dirname, "../../views", "index.html");
+  const template = fs.readFileSync(templatePath, "utf-8");
+
+  const movies = await loadMovies(TMDB_MOVIE_LISTS.nowPlaying);
+  const movie = movies[0] ?? {
+    id: -1,
+    title: "",
+    bannerUrl: "",
+    vote_average: 0,
+  };
+
+  const renderedHTML = template
+    .replace("${MOVIE_ITEMS_PLACEHOLDER}", getMovieItemHTML(movies))
+    .replace("${background-container}", TMDB_BANNER_URL + movie.backdrop_path)
+    .replace("${bestMovie.rate}", round(movie.vote_average, 1))
+    .replace("${bestMovie.title}", movie.title)
+    .replace("${isSelectedNowPlaying}", "selected");
+
+  res.send(renderedHTML);
+});
+
+router.get("/popular", async (_, res) => {
+  const templatePath = path.join(__dirname, "../../views", "index.html");
+  const template = fs.readFileSync(templatePath, "utf-8");
+
+  const movies = await loadMovies(TMDB_MOVIE_LISTS.popular);
+  const movie = movies[0] ?? {
+    id: -1,
+    title: "",
+    bannerUrl: "",
+    vote_average: 0,
+  };
+
+  const renderedHTML = template
+    .replace("${MOVIE_ITEMS_PLACEHOLDER}", getMovieItemHTML(movies))
+    .replace("${background-container}", TMDB_BANNER_URL + movie.backdrop_path)
+    .replace("${bestMovie.rate}", round(movie.vote_average, 1))
+    .replace("${bestMovie.title}", movie.title)
+    .replace("${isSelectedPopular}", "selected");
+
+  res.send(renderedHTML);
+});
+
+router.get("/top-rated", async (_, res) => {
+  const templatePath = path.join(__dirname, "../../views", "index.html");
+  const template = fs.readFileSync(templatePath, "utf-8");
+
+  const movies = await loadMovies(TMDB_MOVIE_LISTS.topRated);
+  const movie = movies[0] ?? {
+    id: -1,
+    title: "",
+    bannerUrl: "",
+    vote_average: 0,
+  };
+
+  const renderedHTML = template
+    .replace("${MOVIE_ITEMS_PLACEHOLDER}", getMovieItemHTML(movies))
+    .replace("${background-container}", TMDB_BANNER_URL + movie.backdrop_path)
+    .replace("${bestMovie.rate}", round(movie.vote_average, 1))
+    .replace("${bestMovie.title}", movie.title)
+    .replace("${isSelectedTopRated}", "selected");
+
+  res.send(renderedHTML);
+});
+
+router.get("/upcoming", async (_, res) => {
+  const templatePath = path.join(__dirname, "../../views", "index.html");
+  const template = fs.readFileSync(templatePath, "utf-8");
+
+  const movies = await loadMovies(TMDB_MOVIE_LISTS.upcoming);
+  const movie = movies[0] ?? {
+    id: -1,
+    title: "",
+    bannerUrl: "",
+    vote_average: 0,
+  };
+
+  const renderedHTML = template
+    .replace("${MOVIE_ITEMS_PLACEHOLDER}", getMovieItemHTML(movies))
+    .replace("${background-container}", TMDB_BANNER_URL + movie.backdrop_path)
+    .replace("${bestMovie.rate}", round(movie.vote_average, 1))
+    .replace("${bestMovie.title}", movie.title)
+    .replace("${isSelectedUpcoming}", "selected");
+
+  res.send(renderedHTML);
+});
+
+const getMovieItemHTML = (movies) => {
+  return movies
+    .map(
+      ({ id, title, vote_average, poster_path }) =>
+        `<li>
+          <a href="/detail/${id}">
+            <div class="item" onClick={onClick}>
+              <img class="thumbnail" src=${
+                TMDB_THUMBNAIL_URL + "/" + poster_path
+              } alt=${title} />
+              <div class="item-desc">
+                <p class="rate">
+                  <img src="../../assets/images/star_empty.png" class="star" />
+                  <span>${round(vote_average, 1)}</span>
+                </p>
+                <strong>${title}</strong>
+              </div>
+            </div>
+          </a>
+        </li>`
+    )
+    .join("");
+};
+
+const BASE_URL = "https://api.themoviedb.org/3/movie";
+const TMDB_MOVIE_LISTS = {
+  popular: BASE_URL + "/popular?language=ko-KR&page=1",
+  nowPlaying: BASE_URL + "/now_playing?language=ko-KR&page=1",
+  topRated: BASE_URL + "/top_rated?language=ko-KR&page=1",
+  upcoming: BASE_URL + "/upcoming?language=ko-KR&page=1",
+};
+
+const TMDB_THUMBNAIL_URL =
+  "https://media.themoviedb.org/t/p/w440_and_h660_face/";
+const TMDB_BANNER_URL =
+  "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/";
+
+const FETCH_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.TMDB_TOKEN,
+  },
+};
+
+const loadMovies = async (url) => {
+  const response = await fetch(url, FETCH_OPTIONS);
+  const data = await response.json();
+
+  return data.results;
+};
+
+const round = (value, decimals = 0) => {
+  const factor = 10 ** decimals;
+
+  return Math.round(value * factor) / factor;
+};
 
 export default router;
