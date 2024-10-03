7d8e213afc4a528b41c254232a251c6fd8ae80d5
chysis <chysiss@naver.com>
2024-10-03 15:08:48 +0900

    feat: 모달을 반환하는 함수 작성 및 상세 페이지 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import generateMovieList from '../utils/generateMovieList.js';
+import loadMovieDetail from '../apis/details.js';
+import renderModal from '../utils/renderModal.js';
 
 const router = Router();
 
@@ -29,6 +31,18 @@ router.get('/top-rated', async (_, res) => {
 
 router.get('/upcoming', async (_, res) => {
   const renderedHTML = await generateMovieList('upcoming');
+
+  res.send(renderedHTML);
+});
+
+router.get('/detail/:movieId', async (req, res) => {
+  const movieId = req.params.movieId;
+  const movieDetail = await loadMovieDetail(movieId);
+
+  const renderedHTML = (await generateMovieList('now-playing')).replace(
+    '<!--${MODAL_AREA}-->',
+    renderModal(movieDetail)
+  );
 
   res.send(renderedHTML);
 });
