e985639acf2fa58084e1391e7911059fdae0ebe6
simhorim <hori5020@gmail.com>
2024-10-03 11:28:28 +0900

    feat(movies): 영화 목록 페이지 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,19 +1,46 @@
 import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import {
+  fetchNowPlayingMovies,
+  fetchPopularMovies,
+  fetchTopRatedMovies,
+  fetchUpcomingMovies,
+  generateRenderedHTML,
+} from "../../src/movies.js";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+router.get("/", async (_, res) => {
+  const movies = await fetchNowPlayingMovies();
+  const renderedHTML = generateRenderedHTML(movies.results, "nowPlaying");
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  res.send(renderedHTML);
+});
+
+router.get("/now-playing", async (_, res) => {
+  const movies = await fetchNowPlayingMovies();
+  const renderedHTML = generateRenderedHTML(movies.results, "nowPlaying");
+
+  res.send(renderedHTML);
+});
+
+router.get("/popular", async (_, res) => {
+  const movies = await fetchPopularMovies();
+  const renderedHTML = generateRenderedHTML(movies.results, "popular");
+
+  res.send(renderedHTML);
+});
+
+router.get("/top-rated", async (_, res) => {
+  const movies = await fetchTopRatedMovies();
+  const renderedHTML = generateRenderedHTML(movies.results, "topRated");
+
+  res.send(renderedHTML);
+});
+
+router.get("/upcoming", async (_, res) => {
+  const movies = await fetchUpcomingMovies();
+  const renderedHTML = generateRenderedHTML(movies.results, "upcoming");
 
   res.send(renderedHTML);
 });
