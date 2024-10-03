21dd160827d42eae3f53c81f187da93733430220
simhorim <hori5020@gmail.com>
2024-10-03 22:52:48 +0900

    feat(modal): 상세 정보 모달 기능 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -5,42 +5,74 @@ import {
   fetchPopularMovies,
   fetchTopRatedMovies,
   fetchUpcomingMovies,
-  generateRenderedHTML,
+  generateRenderedMovieItemsHTML,
 } from "../../src/movies.js";
+import {
+  fetchMovieItemDetail,
+  generateRenderedModalHTML,
+} from "../../src/modal.js";
 
 const router = Router();
 
 router.get("/", async (_, res) => {
   const movies = await fetchNowPlayingMovies();
-  const renderedHTML = generateRenderedHTML(movies.results, "nowPlaying");
+  const renderedHTML = generateRenderedMovieItemsHTML(
+    movies.results,
+    "nowPlaying"
+  );
 
   res.send(renderedHTML);
 });
 
 router.get("/now-playing", async (_, res) => {
   const movies = await fetchNowPlayingMovies();
-  const renderedHTML = generateRenderedHTML(movies.results, "nowPlaying");
+  const renderedHTML = generateRenderedMovieItemsHTML(
+    movies.results,
+    "nowPlaying"
+  );
 
   res.send(renderedHTML);
 });
 
 router.get("/popular", async (_, res) => {
   const movies = await fetchPopularMovies();
-  const renderedHTML = generateRenderedHTML(movies.results, "popular");
+  const renderedHTML = generateRenderedMovieItemsHTML(
+    movies.results,
+    "popular"
+  );
 
   res.send(renderedHTML);
 });
 
 router.get("/top-rated", async (_, res) => {
   const movies = await fetchTopRatedMovies();
-  const renderedHTML = generateRenderedHTML(movies.results, "topRated");
+  const renderedHTML = generateRenderedMovieItemsHTML(
+    movies.results,
+    "topRated"
+  );
 
   res.send(renderedHTML);
 });
 
 router.get("/upcoming", async (_, res) => {
   const movies = await fetchUpcomingMovies();
-  const renderedHTML = generateRenderedHTML(movies.results, "upcoming");
+  const renderedHTML = generateRenderedMovieItemsHTML(
+    movies.results,
+    "upcoming"
+  );
+
+  res.send(renderedHTML);
+});
+
+router.get("/detail/:id", async (req, res) => {
+  const id = req.params.id;
+  const movieItems = await fetchNowPlayingMovies();
+  const movieDetail = await fetchMovieItemDetail(id);
+
+  const renderedHTML = generateRenderedModalHTML(
+    movieItems.results,
+    movieDetail
+  );
 
   res.send(renderedHTML);
 });
