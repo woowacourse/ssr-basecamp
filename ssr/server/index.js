c4d3546631834b380db78e21e93b4a16de9c7953
hwinkr <dnddl8280@naver.com>
2024-10-03 21:03:10 +0900

    chore: ssr routing 수정

--- ssr/server/index.js
+++ ssr/server/index.js
@@ -4,7 +4,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import movieRouter from "./routes/index.js";
+
 // import membersRouter from "./routes/members.js"; // 본 미션 참고를 위한 코드이며 사전 미션에서는 사용하지 않습니다.
+// app.use("/members", membersRouter); // 본 미션 참고를 위한 코드이며 사전 미션에서는 사용하지 않습니다. 뉑~~~
 
 const app = express();
 const PORT = 3000;
@@ -13,9 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use("/assets", express.static(path.join(__dirname, "../public")));
-
 app.use("/", movieRouter);
-// app.use("/members", membersRouter); // 본 미션 참고를 위한 코드이며 사전 미션에서는 사용하지 않습니다.
 
 // Start server
 app.listen(PORT, () => {
--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,21 +1,18 @@
 import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { MOVIE_PAGE_PATH } from "../../constants/path.js";
+
+import { fetchAndRenderMovieDetailModal } from "../handler/movie-detail-modal.js";
+import { fetchAndRenderMoviePage } from "../handler/movie-page.js";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+router.get(MOVIE_PAGE_PATH.home, fetchAndRenderMoviePage);
+router.get(MOVIE_PAGE_PATH.nowPlaying, fetchAndRenderMoviePage);
+router.get(MOVIE_PAGE_PATH.popular, fetchAndRenderMoviePage);
+router.get(MOVIE_PAGE_PATH.topRated, fetchAndRenderMoviePage);
+router.get(MOVIE_PAGE_PATH.upcoming, fetchAndRenderMoviePage);
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
-
-  res.send(renderedHTML);
-});
+router.get(MOVIE_PAGE_PATH.detail, fetchAndRenderMovieDetailModal);
 
 export default router;
