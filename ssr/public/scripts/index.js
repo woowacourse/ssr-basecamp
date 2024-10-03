d7bfab17b14b76390e54e7fb0f5cb57fea5a36b1
00kang <kdb070970@gmail.com>
2024-10-03 19:12:16 +0900

    feat: 영화 리스트 렌더링 구현

--- ssr/public/scripts/index.js
+++ ssr/public/scripts/index.js
@@ -1,1 +1,13 @@
-//
+document.addEventListener("DOMContentLoaded", () => {
+  const tabItems = document.querySelectorAll(".tab-item");
+
+  tabItems.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.classList.add("hover");
+    });
+
+    item.addEventListener("mouseout", () => {
+      item.classList.remove("hover");
+    });
+  });
+});
--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -2,19 +2,103 @@ import { Router } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { fetchMoviesByCategory } from "../apis/movies.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
+const CATEGORIES = {
+  "now-playing": "상영 중",
+  popular: "인기순",
+  "top-rated": "평점순",
+  upcoming: "상영 예정",
+};
+
+const renderMovieItems = (movies) => {
+  return movies
+    .map(
+      (movie) => `
+    <li>
+      <a href="/detail/${movie.id}">
+        <div class="item">
+          <img
+            class="thumbnail"
+            src="https://media.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_path}"
+            alt="${movie.title}"
+          />
+          <div class="item-desc">
+            <p class="rate"><img src="../assets/images/star_empty.png" class="star" /><span>${movie.vote_average.toFixed(
+              1
+            )}</span></p>
+            <strong>${movie.title}</strong>
+          </div>
+        </div>
+      </a>
+    </li>
+  `
+    )
+    .join("");
+};
+
+const renderPage = async (category) => {
   const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+  let template = fs.readFileSync(templatePath, "utf-8");
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  const moviesData = await fetchMoviesByCategory(category);
+  const movieItems = moviesData.results;
+  const moviesHTML = renderMovieItems(movieItems);
 
+  const bestMovie = movieItems[0];
+
+  template = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  template = template.replace(
+    "${background-container}",
+    `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${bestMovie.backdrop_path}`
+  );
+  template = template.replace("${bestMovie.rate}", bestMovie.vote_average.toFixed(1));
+  template = template.replace("${bestMovie.title}", bestMovie.title);
+  template = template.replace("<!--${TAB_ITEMS_PLACEHOLDER}-->", renderTabItems(category));
+
+  return template;
+};
+
+const renderTabItems = (currentCategory) => {
+  return Object.entries(CATEGORIES)
+    .map(
+      ([key, value]) => `
+    <li>
+      <a href="/${key}" class="${
+        currentCategory.toLowerCase().replace("_", "-") === key ? "selected" : ""
+      }">
+        <div class="tab-item">
+          <h3>${value}</h3>
+        </div>
+      </a>
+    </li>
+  `
+    )
+    .join("");
+};
+
+router.get(["/", "/now-playing"], async (_, res) => {
+  const renderedHTML = await renderPage("NOW_PLAYING");
+  res.send(renderedHTML);
+});
+
+router.get("/popular", async (_, res) => {
+  const renderedHTML = await renderPage("POPULAR");
+  res.send(renderedHTML);
+});
+
+router.get("/top-rated", async (_, res) => {
+  const renderedHTML = await renderPage("TOP_RATED");
+  res.send(renderedHTML);
+});
+
+router.get("/upcoming", async (_, res) => {
+  const renderedHTML = await renderPage("UPCOMING");
   res.send(renderedHTML);
 });
 
