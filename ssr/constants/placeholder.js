3edfd0fe0a326c2317ac05422389556242832b74
hwinkr <dnddl8280@naver.com>
2024-10-03 21:01:55 +0900

    chore: 영화 페이지를 그리기 위해 사용되는 매직넘버 상수화
    - window location path, api endpoint, replace placeholder, movie tabs

---
+++ ssr/constants/placeholder.js
@@ -0,0 +1,9 @@
+export const HTML_PLACEHOLDERS = {
+  movieList: "<!--${MOVIE_ITEMS_PLACEHOLDER}-->",
+  movieTabs: "<!--${MOVIE_TABS}-->",
+  movieDetailModal: "<!--${MODAL_AREA}-->",
+  bestMovieDetailTrigger: "<!--${BEST_MOVIE_DETAIL_TRIGGER}-->",
+  bestMovieRate: "${bestMovie.rate}",
+  bestMovieTitle: "${bestMovie.title}",
+  bestMovieBackgroundImage: "${background-container}",
+};
---
+++ ssr/constants/tabs.js
@@ -0,0 +1,18 @@
+export const MOVIE_TABS = [
+  {
+    href: "/now-playing",
+    label: "상영 중",
+  },
+  {
+    href: "/popular",
+    label: "인기순",
+  },
+  {
+    href: "/top-rated",
+    label: "평점순",
+  },
+  {
+    href: "/upcoming",
+    label: "상영 에정",
+  },
+];
