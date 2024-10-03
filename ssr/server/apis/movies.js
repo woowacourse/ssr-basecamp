47092c2185024b596261f28b096eea77c2e5c213
00kang <kdb070970@gmail.com>
2024-10-03 20:16:46 +0900

    feat: 모달 렌더링 구현

--- ssr/server/apis/movies.js
+++ ssr/server/apis/movies.js
@@ -1,7 +1,8 @@
 import { TMDB_MOVIE_LISTS, TMDB_MOVIE_DETAIL_URL, FETCH_OPTIONS } from "../constants.js";
 
 export const fetchMoviesByCategory = async (category) => {
-  const response = await fetch(TMDB_MOVIE_LISTS[category], FETCH_OPTIONS);
+  const dd = category === "" ? "NOW_PLAYING" : category;
+  const response = await fetch(TMDB_MOVIE_LISTS[dd], FETCH_OPTIONS);
   const data = await response.json();
 
   return data;
--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { fetchMoviesByCategory } from "../apis/movies.js";
+import { fetchMoviesByCategory, fetchDetailMovie } from "../apis/movies.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -42,7 +42,7 @@ const renderMovieItems = (movies) => {
     .join("");
 };
 
-const renderPage = async (category) => {
+const renderPage = async ({ category, id }) => {
   const templatePath = path.join(__dirname, "../../views", "index.html");
   let template = fs.readFileSync(templatePath, "utf-8");
 
@@ -53,6 +53,7 @@ const renderPage = async (category) => {
   const bestMovie = movieItems[0];
 
   template = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+
   template = template.replace(
     "${background-container}",
     `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${bestMovie.backdrop_path}`
@@ -60,8 +61,54 @@ const renderPage = async (category) => {
   template = template.replace("${bestMovie.rate}", bestMovie.vote_average.toFixed(1));
   template = template.replace("${bestMovie.title}", bestMovie.title);
   template = template.replace("<!--${TAB_ITEMS_PLACEHOLDER}-->", renderTabItems(category));
+  if (id) {
+    template = template.replace("<!--${MODAL_AREA}-->", await renderMovieItemModal(id));
+  }
 
   return template;
+};
+
+const renderMovieItemModal = async (id) => {
+  const movie = await fetchDetailMovie(id);
+  const genres = movie.genres && movie.genres.map((genre) => genre.name).join(", ");
+
+  return `
+    <div class="modal-background active" id="modalBackground">
+      <div class="modal">
+        <button class="close-modal" id="closeModal"><img src="../assets/images/modal_button_close.png" /></button>
+        <div class="modal-container">
+          <div class="modal-image">
+            <img src="https://image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}" />
+          </div>
+          <div class="modal-description">
+            <h2>${movie.title}</h2>
+            <p class="category">
+              ${movie.release_date} · ${genres}
+            </p>
+            <p class="rate">
+              <img src="../assets/images/star_filled.png" class="star" />
+              <span>${movie.vote_average}</span>
+            </p>
+            <hr />
+            <p class="detail">${movie.overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 모달 창 닫기 스크립트 -->
+    <script>
+        const modalBackground = document.getElementById("modalBackground");
+        const closeModal = document.getElementById("closeModal");
+        const previousPath = new URL(document.referrer).pathname;
+
+        document.addEventListener("DOMContentLoaded", () => {
+          closeModal.addEventListener("click", () => {
+            modalBackground.classList.remove("active");
+            history.replaceState({}, '', previousPath);
+          });
+        });
+      </script>
+  `;
 };
 
 const renderTabItems = (currentCategory) => {
@@ -83,22 +130,31 @@ const renderTabItems = (currentCategory) => {
 };
 
 router.get(["/", "/now-playing"], async (_, res) => {
-  const renderedHTML = await renderPage("NOW_PLAYING");
+  const renderedHTML = await renderPage({ category: "NOW_PLAYING", id: null });
   res.send(renderedHTML);
 });
 
 router.get("/popular", async (_, res) => {
-  const renderedHTML = await renderPage("POPULAR");
+  const renderedHTML = await renderPage({ category: "POPULAR", id: null });
   res.send(renderedHTML);
 });
 
 router.get("/top-rated", async (_, res) => {
-  const renderedHTML = await renderPage("TOP_RATED");
+  const renderedHTML = await renderPage({ category: "TOP_RATED", id: null });
   res.send(renderedHTML);
 });
 
 router.get("/upcoming", async (_, res) => {
-  const renderedHTML = await renderPage("UPCOMING");
+  const renderedHTML = await renderPage({ category: "UPCOMING", id: null });
+  res.send(renderedHTML);
+});
+
+router.get("/detail/:id", async (req, res) => {
+  const previousPage = req.get("Referrer") || req.header("Referrer");
+  const category = previousPage.split("/").at(-1).toUpperCase().replace("-", "_");
+  const movieId = req.params.id;
+
+  const renderedHTML = await renderPage({ category, id: movieId });
   res.send(renderedHTML);
 });
 
