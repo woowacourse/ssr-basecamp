796b96f6c7ac0d78130f9e2292dfed20e3e7968d
Seongjin Hong <hongseongjin.to@gmail.com>
2024-10-03 15:55:40 +0900

    feat: 리소스, API 주소 및 경로 상수 추가

---
+++ ssr/server/constant.js
@@ -0,0 +1,31 @@
+export const BASE_URL = "https://api.themoviedb.org/3/movie";
+
+export const TMDB_RESOURCE = {
+  IMAGE: {
+    THUMBNAIL_URL: "https://media.themoviedb.org/t/p/w440_and_h660_face/",
+    ORIGINAL_URL: "https://image.tmdb.org/t/p/original/",
+    BANNER_URL: "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/",
+  },
+};
+
+export const TMDB_API_URL = {
+  MOVIE_LISTS: {
+    POPULAR: BASE_URL + "/popular?language=ko-KR&page=1",
+    NOW_PLAYING: BASE_URL + "/now_playing?language=ko-KR&page=1",
+    TOP_RATED: BASE_URL + "/top_rated?language=ko-KR&page=1",
+    UPCOMING: BASE_URL + "/upcoming?language=ko-KR&page=1",
+  },
+  MOVIE_DETAIL_URL: BASE_URL,
+};
+
+export const PATH = {
+  MOVIE_DETAIL: (movieId) => `/detail/${movieId}`,
+};
+
+export const FETCH_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.TMDB_TOKEN,
+  },
+};
