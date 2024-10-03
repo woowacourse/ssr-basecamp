847564ca722c4ca78d994340cbf2839ef0e9a117
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-10-03 18:42:41 +0900

    feat: 영화 목록 페이지 구현

--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -1,19 +1,130 @@
-import { Router } from "express";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import { Router } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fetchMovies, renderMovieItem } from './utils.js';
+import { TMDB_BANNER_URL, TMDB_MOVIE_LISTS } from '../src/constant.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
-  const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+router.get('/', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../views', 'index.html');
 
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  const responseFetchMovies = await fetchMovies(TMDB_MOVIE_LISTS.NOW_PLAYING);
+  const movieList = responseFetchMovies.results;
+
+  const moviesHTML = renderMovieItem(movieList).join('');
+
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const renderedHTML = template
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', moviesHTML)
+    .replace(
+      '${background-container}',
+      `${TMDB_BANNER_URL}${movieList[0].backdrop_path}`
+    )
+    .replace('${bestMovie.rate}', movieList[0].vote_average)
+    .replace('${bestMovie.title}', movieList[0].title)
+    .replace('${TAB_NOW_PLAYING}', 'selected');
+
+  res.send(renderedHTML);
+});
+
+// 상영 중
+router.get('/now-playing', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../views', 'index.html');
+
+  const responseFetchMovies = await fetchMovies(TMDB_MOVIE_LISTS.NOW_PLAYING);
+  const movieList = responseFetchMovies.results;
+
+  const moviesHTML = renderMovieItem(movieList).join('');
+
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const renderedHTML = template
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', moviesHTML)
+    .replace(
+      '${background-container}',
+      `${TMDB_BANNER_URL}${movieList[0].backdrop_path}`
+    )
+    .replace('${bestMovie.rate}', movieList[0].vote_average)
+    .replace('${bestMovie.title}', movieList[0].title)
+    .replace('${TAB_NOW_PLAYING}', 'selected');
+
+  res.send(renderedHTML);
+});
+
+//인기순
+router.get('/popular', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../views', 'index.html');
+
+  const responseFetchMovies = await fetchMovies(TMDB_MOVIE_LISTS.POPULAR);
+  const movieList = responseFetchMovies.results;
+
+  const moviesHTML = renderMovieItem(movieList).join('');
+
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const renderedHTML = template
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', moviesHTML)
+    .replace(
+      '${background-container}',
+      `${TMDB_BANNER_URL}${movieList[0].backdrop_path}`
+    )
+    .replace('${bestMovie.rate}', movieList[0].vote_average)
+    .replace('${bestMovie.title}', movieList[0].title)
+    .replace('${TAB_POPULAR}', 'selected');
+
+  res.send(renderedHTML);
+});
+
+// 평점순
+router.get('/top-rated', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../views', 'index.html');
+
+  const responseFetchMovies = await fetchMovies(TMDB_MOVIE_LISTS.TOP_RATED);
+  const movieList = responseFetchMovies.results;
+
+  const moviesHTML = renderMovieItem(movieList).join('');
+
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const renderedHTML = template
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', moviesHTML)
+    .replace(
+      '${background-container}',
+      `${TMDB_BANNER_URL}${movieList[0].backdrop_path}`
+    )
+    .replace('${bestMovie.rate}', movieList[0].vote_average)
+    .replace('${bestMovie.title}', movieList[0].title)
+    .replace('${TAB_TOP_RATED}', 'selected');
+
+  res.send(renderedHTML);
+});
+
+// 상영 예정
+router.get('/upcoming', async (_, res) => {
+  const templatePath = path.join(__dirname, '../../views', 'index.html');
+
+  const responseFetchMovies = await fetchMovies(TMDB_MOVIE_LISTS.UPCOMING);
+  const movieList = responseFetchMovies.results;
+
+  const moviesHTML = renderMovieItem(movieList).join('');
+
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const renderedHTML = template
+    .replace('<!--${MOVIE_ITEMS_PLACEHOLDER}-->', moviesHTML)
+    .replace(
+      '${background-container}',
+      `${TMDB_BANNER_URL}${movieList[0].backdrop_path}`
+    )
+    .replace('${bestMovie.rate}', movieList[0].vote_average)
+    .replace('${bestMovie.title}', movieList[0].title)
+    .replace('${TAB_UPCOMING}', 'selected');
 
   res.send(renderedHTML);
 });
